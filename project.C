44dde704159d2db2f021a5337d366190ff8327eb
Sachin U Krishna <89995107+Sachin-U-Krishna@users.noreply.github.com>
2022-08-03 23:37:17 +0530

    Added admin base layout and debug

--- project.C
+++ project.C
@@ -22,10 +22,11 @@ char name[8][20]={
 
 void main(){
 	int choice,option;
-
+	printf("*************** Welcome to Christ Library ***************\n\n");
     while(1){
-    	printf("*************** Welcome to Christ Library ***************\n");
-    	printf("\n1. Student checkin\n2. Admin Login\n3. Exit Library\n");
+    	printf("-------- Main Menu --------\n");
+    	printf("\n1. Student check-in\n2. Admin Login\n3. Exit Library\n\n");
+    	printf("---------------------------");
 	    printf("\nSelect option: ");
 	    scanf("%d",&option);
 	    system("cls");
@@ -34,23 +35,29 @@ void main(){
 	    	case 1:
 	    		//Student Checkin code
 	    		while(option==1){
+	    			printf("-------- Student Menu --------\n\n");
 	    			printf("1  Add Student Details \n");
-				    printf("2. for printing detail of student  \n");
+				    printf("2. Display Student Detail  \n");
 				    printf("3. Display books \n");
 				    printf("4. Borrow a book \n");
-				    printf("5. Exit \n ");
-				    printf("\nEnter choice: ");
+				    printf("5. Exit \n\n");
+				    printf("------------------------------\n");
+				    printf("Enter choice: ");
 				    scanf("%d",&choice);
 				    switch(choice)
 				    {
-				        case 1: student_detail();
-				        break;
-				        case 2: print_detail();
-				        break;
-				        case 3: print_book();
-				        break;
-				        case 4: borrow();
-				        break;
+				        case 1: 
+							student_detail();
+				        	break;
+				        case 2: 
+							print_detail();
+				        	break;
+				        case 3: 
+							print_book();
+				        	break;
+				        case 4: 
+							borrow();
+				        	break;
 				        case 5:
 							system("cls"); 
 							option=0;
@@ -64,10 +71,35 @@ void main(){
 			case 2:
 	    		//Admin Login code
 	    		while(option==2){
-	    			
+	    			printf("--------- Admin Menu ---------\n\n");
+	    			printf("1  Add Books \n");
+				    printf("2. Extra option  \n");
+				    printf("3. Exit \n\n");
+				    printf("------------------------------\n");
+				    printf("Enter choice: ");
+				    scanf("%d",&choice);
+				    switch(choice)
+				    {
+				        case 1: 
+							add_book();
+				        	break;
+				        case 2: 
+				        	printf("New");
+							//code if needed
+				        	break;
+				        case 3:
+							system("cls"); 
+							option=0;
+				        	break;
+						default:
+							system("cls"); 
+							printf("\nInvalid choice.\n\n");
+					}
 				}
 				break;
 			case 3:
+				system("cls");
+				printf("Thank You!");
 				exit(1);
 				break;
 			default:
@@ -79,11 +111,11 @@ void main(){
 //Function Definitions
 void student_detail()
 {
-    printf(" Enter the student name ");
+    printf("Enter Student name: ");
     scanf("%s",stud_name);
-    printf("\n Enter the student id");
+    printf("\nEnter Student ID: ");
     scanf("%d",&stud_id);
-    printf("\n Enter the programme you are enrolled");
+    printf("\n Enter the programme Enrolled: ");
     scanf("%s",stud_programme);
 }
 
@@ -91,7 +123,7 @@ void print_detail()
 {
     printf(" Name - %s \n",stud_name);
     printf(" Id - %d \n",stud_id);
-    printf(" Programme - %s \n ", stud_programme);
+    printf(" Programme - %s \n", stud_programme);
 }
 
 void print_book()
